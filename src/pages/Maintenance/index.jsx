60adb6adbb8ff0c98f6df34dcf02085ae135e0ea
stephanietako <gabeshine@live.fr>
2023-12-11 12:16:06 +0100

    refacto

--- src/pages/Maintenance/index.jsx
+++ src/pages/Maintenance/index.jsx
@@ -1,10 +1,17 @@
 // Styles
 import styles from "./styles.module.scss";
 // Assets
+import logo from "../../assets/logo/jmlogo.jpg";
 const Maintenance = () => {
   return (
     <div className={styles.maintenance}>
       <div className={styles.__container}>
+        <div className={styles.__logo}>
+          <img
+            src={logo}
+            alt="Jean-Marc Eliette graphiste motion design logo "
+          />
+        </div>
         <div className={styles.__txt}>
           <p>Bientôt le site de Jean-Marc Eiliette</p>
           <p>Développement en cours....</p>
